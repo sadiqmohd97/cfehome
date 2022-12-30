8640f34f54c24310f743354b5066552f1eb8473d
sadiq <sadiq.mohammad@paysense.in>
2022-12-31 00:29:46 +0530

    Getting params and body from httpRequest

--- api/views.py
+++ api/views.py
@@ -1,7 +1,25 @@
 from django.shortcuts import render
-from django.http import JsonResponse
+from django.http import JsonResponse,HttpRequest
+import json
 
 # Create your views here.
  
-def index(request):
-    return JsonResponse({'message':'Hi'})
+def index(request:HttpRequest):
+    print(request)
+    body = request.body  #byte string of json
+    data = {}
+    print(request.GET) #QueryDict
+    data['params'] = request.GET
+    try:
+        data = json.loads(body) #dictionary
+    except:
+        pass
+    print(data)
+    print(type(data))
+    print(data.keys())
+    data['params'] = request.GET
+    data['headers'] = dict(request.headers)
+    data['content-type'] = request.content_type
+    return JsonResponse(data) 
+
+  
